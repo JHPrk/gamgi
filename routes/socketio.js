5c05d049b0d40b37adafa994a839841cb773944b
JHPrk <qkrwogus1236@gmail.com>
2017-09-09 20:05:04 +0900

    socket_chatter_function

--- routes/socketio.js
+++ routes/socketio.js
@@ -1,19 +1,67 @@
+var debug = require('./debugTool');
 var io;
 
-exports.setServerToIO = function(server){ 
+exports.setServerToIO = function(server,callback){ 
 	io = require('socket.io')(server);
 	io.emit('some event', {for:'everyone'});
-	io.on('connection', function(socket){
-  		console.log('a user connected');
+	/*io.on('connection', function(socket){
+		socket.on('join', function(roomid){
+			socket.join(roomid);
+		});
+  		debug.log('a user connected');
   		socket.broadcast.emit('hi');
   		socket.on('chat message', function(msg){
     		io.emit('chat message',msg);
-   			console.log('message: ' + msg);
+   			debug.log('message: ' + msg);
  	 	})
  		socket.on('disconnected', function(){
-    		console.log('user disconnected');
+    		debug.log('user disconnected');
   		});
+	});*/
+	io.on('connection',function(socket){
+		debug.log('a user connected');
+  		socket.broadcast.emit('hi');
+		socket.on('join', function(roomid, nickname){
+			socket.join(roomid);
+			socket.nickname = nickname;
+			// debug.log(nickname);
+			socket.roomid = roomid;
+			// debug.log(roomid);
+			debug.log(Object.keys(io.sockets.adapter.rooms[roomid].sockets));
+			io.to(roomid).emit('notify', nickname + '님이 방에 입장하였습니다.');
+			io.to(roomid).emit('user cnt', Object.keys(io.sockets.adapter.rooms[roomid].sockets).length);
+		});
+		socket.on('chat message', function(msg){
+			io.to(socket.roomid).emit('chat message',msg,socket.nickname);
+			//debug.log(socket.roomid);
+			// debug.log(socket.nickname);
+			debug.log('message: ' + msg);
+		});
+		socket.on('disconnect', function(){
+			debug.log('user disconnected');
+			// debug.log(socket.nickname);
+			io.to(socket.roomid).emit('notify',socket.nickname + '님이 방을 떠났습니다.');
+			io.to(socket.roomid).emit('user cnt', Object.keys(io.sockets.adapter.rooms[socket.roomid].sockets).length);
+		});
 	});
+}
+exports.setIOnsp = function(nsp,callback){
+	nsp.on('connection',function(socket){
+		debug.log('a user connected');
+		nsp.on('join', function(roomid, nickname){
+			socket.join(roomid);
+			socket.nickname = nickname;
+			io.emit('notify', socket.nickname + '님이 방에 입장하였습니다.');
+		})
+		socket.on('chat message', function(msg){
+			nsp.emit('chat message',msg);
+			debug.log('message: ' + msg);
+		})
+		socket.on('disconnected', function(){
+			debug.log('user disconnected');
+			nsp.emit('notify',socket.nickname + '님이 방을 떠났습니다.');
+		})
+	})
 }
 
 exports.getIO = function(callback)
@@ -21,7 +69,7 @@ exports.getIO = function(callback)
 	if(!io)
 	{
 		var err = new Error("bad Socket Lib!");
-		console.log('hello');
+		debug.log('hello');
 		return callback(err);
 	}
 	return callback(null,io);
